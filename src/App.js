63142ff691c2f88f242466ea56c48a9975bfe133
Asfakur <saikat.hap@gmail.com>
2021-03-17 23:57:16 +0600

    42_5-1 Implement Simple Google authentication system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+// import "firebase/firestore";
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const [user, setUser] = useState({})
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+
+  const handleGoogleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        
+        var credential = result.credential;
+
+        
+        var token = credential.accessToken;
+        
+        var user = result.user;
+        console.log(user);
+        setUser(user);
+        
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log(errorCode, errorMessage);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Sign in using google</button>
+      <h3>Email : {user.email}</h3>
+      <img src={user.photoURL} alt=""/>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgrqGdKDfumWrhDtb_d6sAOReULs4v3Cg",
+    authDomain: "auth-recap-42-5.firebaseapp.com",
+    projectId: "auth-recap-42-5",
+    storageBucket: "auth-recap-42-5.appspot.com",
+    messagingSenderId: "455584039975",
+    appId: "1:455584039975:web:4965ec32ddb51bda157a02"
+};
+export default firebaseConfig;
