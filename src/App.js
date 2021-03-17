03beb58736d5aa376dc040827b1cc5cb33afd1db
Asfakur <saikat.hap@gmail.com>
2021-03-18 00:39:38 +0600

    42_5-3 Implement github authentication system

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ function App() {
 
   var fbProvider = new firebase.auth.FacebookAuthProvider();
 
+  var gitProvider = new firebase.auth.GithubAuthProvider();
+
 
   const handleGoogleSignIn = () => {
     firebase.auth()
@@ -54,7 +56,7 @@ function App() {
       .auth()
       .signInWithPopup(fbProvider)
       .then((result) => {
-      
+
         var credential = result.credential;
 
         var user = result.user;
@@ -73,11 +75,36 @@ function App() {
         console.log(errorCode, errorMessage, email, credential);
       });
   }
+
+  const handleGithubSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(gitProvider)
+      .then((result) => {
+        
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log('git user', user);
+        setUser(user);
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log('error', errorCode, errorMessage, email, credential);
+      });
+
+  }
+
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Sign in using google</button>
       <br />
       <button onClick={handleFacebookSignIn}>Sign in using Facebook</button>
+      <br />
+      <button onClick={handleGithubSignIn}>Sign in using Github</button>
       <h3>Email : {user.email}</h3>
       <h3>Display Name : {user.displayName}</h3>
       <img src={user.photoURL} alt="" />
