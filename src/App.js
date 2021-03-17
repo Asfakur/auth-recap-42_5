2dc68cb5b854545e5387dd8f1aed2679f11c9bcf
Asfakur <saikat.hap@gmail.com>
2021-03-18 00:22:30 +0600

    42_5-2 Implement Facebook authentication

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import firebaseConfig from './firebase.config';
 import { useState } from 'react';
 // import "firebase/firestore";
 
-firebase.initializeApp(firebaseConfig);
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
 function App() {
 
@@ -13,21 +19,23 @@ function App() {
 
   var provider = new firebase.auth.GoogleAuthProvider();
 
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+
 
   const handleGoogleSignIn = () => {
     firebase.auth()
       .signInWithPopup(provider)
       .then((result) => {
-        
+
         var credential = result.credential;
 
-        
+
         var token = credential.accessToken;
-        
+
         var user = result.user;
         console.log(user);
         setUser(user);
-        
+
       }).catch((error) => {
         // Handle Errors here.
         var errorCode = error.code;
@@ -40,11 +48,39 @@ function App() {
         console.log(errorCode, errorMessage);
       });
   }
+
+  const handleFacebookSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+      
+        var credential = result.credential;
+
+        var user = result.user;
+        var accessToken = credential.accessToken;
+        console.log('fb user', user);
+        setUser(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  }
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Sign in using google</button>
+      <br />
+      <button onClick={handleFacebookSignIn}>Sign in using Facebook</button>
       <h3>Email : {user.email}</h3>
-      <img src={user.photoURL} alt=""/>
+      <h3>Display Name : {user.displayName}</h3>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
